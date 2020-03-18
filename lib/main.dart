e6cc61ef04b0fb5b79163c3e8334160704a272b0
tanjs <tjs_95@163.com>
2020-03-18 15:34:15 +0800

    功能列表下拉刷新与上拉加载更多功能实现

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
-const CITY_NAMES = [ '北京', '上海', '广州', '深圳', '杭州', '苏州', '成都', '武汉', '郑州', '洛阳', '厦门', '青岛', '拉萨' ];
+List<String> cityNames = [ '北京', '上海', '广州', '深圳', '杭州', '苏州', '成都', '武汉', '郑州', '洛阳', '厦门', '青岛', '拉萨' ];
 
 class MyApp extends StatefulWidget {
   @override
@@ -9,23 +9,56 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  final title = '网格布局';
+  ScrollController _scrollController = ScrollController();
+  @override
+  void initState() {
+    _scrollController.addListener(() {
+      if(_scrollController.position.pixels == _scrollController.position.maxScrollExtent) {
+        _loadData();
+      }
+    });
+    super.initState();
+  }
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
   @override
   Widget build(BuildContext context) {
+    final title = '高级功能列表下拉刷新与上拉加载更多功能实现';
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text(title),
         ),
-        body: GridView.count(
-          crossAxisCount: 2,
-          children: _buildList(),
-        ),
+        body: RefreshIndicator(
+          onRefresh: _handleRefresh,
+          child: ListView(
+            controller: _scrollController,
+            children: _buildList(),
+          ),
+        )
       ),
     );
   }
+  _loadData() async {
+    await Future.delayed(Duration(seconds: 200));
+    setState(() {
+      List<String> list = List<String>.from(cityNames);
+      list.addAll(cityNames);
+      cityNames = list;
+    });
+  }
+  Future<Null> _handleRefresh() async {
+    await Future.delayed(Duration(seconds: 2));
+    setState(() {
+      cityNames = cityNames.reversed.toList();
+    });
+    return null;
+  }
   List<Widget> _buildList() {
-    return CITY_NAMES.map((city) => _item(city)).toList();
+    return cityNames.map((city) => _item(city)).toList();
   }
   Widget _item(String city) {
     return Container(
