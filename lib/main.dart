70888b43cf95aa9d858c41490930c6f486b258b7
tanjs <tjs_95@163.com>
2020-03-18 14:35:58 +0800

    列表展开与收起

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
-const CITY_NAMES = [ '北京', '上海', '广州', '深圳', '杭州', '苏州', '成都', '武汉', '郑州', '洛阳', '厦门', '青岛', '拉萨' ];
+const CITY_NAMES = {
+  '北京': ['东城区', '西城区', '朝阳区', '丰台区', '石景山区', '海淀区', '顺义区'],
+  '上海': ['黄浦区', '徐汇区', '长宁区', '静安区', '普陀区', '闸北区', '虹口区'],
+  '广州': ['越秀', '海珠', '荔湾', '天河', '白云', '黄埔', '南沙', '番禺'],
+  '深圳': ['南山', '福田', '罗湖', '盐田', '龙岗', '宝安', '龙华'],
+  '杭州': ['上城区', '下城区', '江干区', '拱墅区', '西湖区', '滨江区'],
+  '苏州': ['姑苏区', '吴中区', '相城区', '高新区', '虎丘区', '工业园区', '吴江区']
+};
 
 class MyApp extends StatefulWidget {
   @override
@@ -14,30 +21,38 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('组件使用'),
+          title: Text('列表展开与收起'),
         ),
-        body: Container(
-          height: 100,
-          child: ListView(
-            scrollDirection: Axis.horizontal,
-            children: _buildList(),
-          ),
-        )
+        body: ListView(
+          children: _buildList(),
+        ),
       ),
     );
   }
   List<Widget> _buildList() {
-    return CITY_NAMES.map((city) => _item(city)).toList();
+    List<Widget> widget = [];
+    CITY_NAMES.keys.forEach((key) {
+      widget.add(_item(key, CITY_NAMES[key]));
+    });
+    return widget;
   }
-  Widget _item(city) {
-    return Container(
-      width: 160,
-      alignment: Alignment.center,
-      margin: EdgeInsets.only(right: 5),
-      decoration: BoxDecoration(color: Colors.teal),
-      child: Text(
+  Widget _item(String city, List<String> subCities) {
+    return ExpansionTile(
+      title: Text(
         city,
-        style: TextStyle(color: Colors.white, fontSize: 20),
+        style: TextStyle(color: Colors.black54, fontSize: 20),
+      ),
+      children: subCities.map((subCity) => _buildSub(subCity)).toList()
+    );
+  }
+  Widget _buildSub(String subCity) {
+    return FractionallySizedBox(
+      widthFactor: 1,
+      child: Container(
+        height: 50,
+        margin: EdgeInsets.only(bottom: 5),
+//        decoration: BoxDecoration(color: Colors.lightBlueAccent),
+        child: Text(subCity),
       ),
     );
   }
